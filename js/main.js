b21a66453b72f639cdfef051d80dd4ad5e8e7c68
Mehedi <mehedihasansagor301@gmail.com>
2024-09-23 04:36:24 +0600

    complete header section & challenge blog part

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.getElementById('history-button').addEventListener('click',function(){
+    document.getElementById('donation-button').classList.remove('bg-[#B4F461]');
+    document.getElementById('history-button').classList.add('bg-[#B4F461]');
+})
+
+document.getElementById('donation-button').addEventListener('click',function(){
+    document.getElementById('history-button').classList.remove('bg-[#B4F461]');
+    document.getElementById('donation-button').classList.add('bg-[#B4F461]');
+})
